61e3211faf4c64c5139826edbeabc3068be64b19
Bruno Lima <brunoluislima19@gmail.com>
2024-02-07 22:54:26 -0300

    feat: model postschema e route get post id

--- app/api.py
+++ app/api.py
@@ -1,7 +1,38 @@
 from fastapi import FastAPI
+from app.model import PostSchema
+
+posts = [
+    {
+        "id": 1,
+        "title": "Pancake",
+        "content": "Lorem Ipsum ..."
+    }
+]
+
+users = []
 
 app = FastAPI()
+
 
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return {"message": "Welcome to yout blog"}
+
+
+@app.get("/posts", tags=["posts"])
+async def get_posts() -> dict:
+    return { "data": posts }
+
+
+@app.get("/posts/{id}", tags=["posts"])
+async def get_single_post(id: int) -> dict:
+    if id > len(posts):
+        return {
+            "error": "No such post with the supplied ID."
+        }
+
+    for post in posts:
+        if post["id"] == id:
+            return {
+                "data": posts
+            }
