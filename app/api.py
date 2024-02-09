d86ff84be5a15f54b30c1d9fed30259fc4d1c3e0
Bruno Lima <brunoluislima19@gmail.com>
2024-02-09 20:13:00 -0300

    feat: route login

--- app/api.py
+++ app/api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Body
+from fastapi.exceptions import WebSocketRequestValidationError
 
 from app.model import PostSchema, UserSchema, UserLoginSchema
 from app.auth.auth_handler import signJWT
@@ -53,3 +54,19 @@ async def add_post(post: PostSchema) -> dict:
 async def create_user(user: UserSchema = Body(...)):
     users.append(user)
     return signJWT(user.email)
+
+
+def check_user(data: UserLoginSchema):
+    for user in users:
+        if user.email == data.email and user.password == data.password:
+            return True
+    return False
+
+
+@app.post("/user/login", tags=["user"])
+async def user_login(user: UserLoginSchema = Body(...)):
+    if check_user(user):
+        return signJWT(user.email)
+    return {
+        "error": "Wrong login details!"
+    }
